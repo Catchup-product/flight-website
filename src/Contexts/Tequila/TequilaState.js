5156dc2fadfe4c5c8377d9356be0d871737696c1
B0295868 <shaurya.pratapsingh@airtel.com>
2024-04-16 00:28:57 +0530

    token api integrated

--- src/Contexts/Tequila/TequilaState.js
+++ src/Contexts/Tequila/TequilaState.js
@@ -2,100 +2,43 @@ import React, { useState } from "react";
 import TequilaContext from "./TequilaContext";
 
 const TequilaState = (props) => {
-  const HOST = "https://api.tequila.kiwi.com";
+  const AMADEUS_HOST = "https://test.api.amadeus.com";
 
-  const [routeData, setRouteData] = useState([]);
-  const [fligthData, setFligthData] = useState(null);
-  const [airportSuggestions, setAirportSuggestions] = useState([]);
+  const [amadeusToken, setAmadeusToken] = useState(null);
 
-// In TequilaState
-const searchItinery = async ({ apikey, flyFrom, flyTo, dateFrom, dateTo }) => {
-  const response = await fetch(`${HOST}/v2/search?fly_from=${flyFrom}&fly_to=${flyTo}&date_from=${dateFrom}&date_to=${dateTo}`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      apikey,
-    },
-  });
-
-  const json = await response.json();
-  if (response.status === 200) {
-    return json; // Return the entire JSON response
-  } else {
-    throw new Error(`API request failed with status ${response.status}`);
-  }
-};
-
-  // Check Flights
-  const checkFlights = async ({
-    apikey,
-    bookingToken,
-    bags,
-    adults,
-    children,
-    infants,
-    sessionId,
-  }) => {
-    const response = await fetch(
-      HOST +
-        "/v2/booking/check_flights?booking_token=" +
-        bookingToken +
-        "&bnum=" +
-        bags +
-        "&adults=" +
-        adults +
-        "&children=" +
-        children,
-      "&infants=" + infants,
-      "&session_id=" + sessionId,
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          apikey,
-        },
-      }
-    );
-    const json = await response.json();
-    if (response.status === 200) {
-      setFligthData(...json);
-    }
-  };
-
-  // Airport Suggestions
-  const suggestAirport = async ({ apikey, term }) => {
+  const fetchAmadeusToken = async ({ clientId, clientSecret }) => {
     try {
-      const response = await fetch(`${HOST}/locations/query?term=${term}`, {
-        method: "GET",
+      const response = await fetch(`${AMADEUS_HOST}/v1/security/oauth2/token`, {
+        method: "POST",
         headers: {
-          "accept": "application/json",
-          "apikey": apikey,
+          "Content-Type": "application/x-www-form-urlencoded",
         },
+        body: new URLSearchParams({
+          client_id: clientId,
+          client_secret: clientSecret,
+          grant_type: "client_credentials",
+        }),
       });
-  
+
       if (!response.ok) {
-        throw new Error(`API request failed with status ${response.status}`);
+        throw new Error(`Failed to fetch token. Status: ${response.status}`);
       }
-  
-      const json = await response.json();
-      console.log(json); // Log to verify the structure
-      return json.locations; // Return the locations part of the response
+
+      const data = await response.json();
+      setAmadeusToken(data.access_token);
+      console.log("token"+amadeusToken);
     } catch (error) {
-      console.error("Error fetching airport suggestions:", error);
-      return []; // Return an empty array in case of error
+      console.error("Error fetching Amadeus token:", error);
     }
   };
-  
+
+
 
   return (
     <TequilaContext.Provider
       value={{
-        routeData,
-        fligthData,
-        airportSuggestions,
-        searchItinery,
-        checkFlights,
-        suggestAirport,
+        amadeusToken,
+        fetchAmadeusToken,
       }}
     >
       {props.children}
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState,useContext,useCallback } from 'react';
+import React, { useState,useContext,useCallback,useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlaneDeparture, faPlaneArrival, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { DateRangePicker } from 'react-date-range';
@@ -25,40 +25,13 @@ const SearchForm = () => {
   
 
   const [airportSuggestions, setAirportSuggestions] = useState([]);
-  const { suggestAirport, searchItinery } = useContext(TequilaContext);
+  const { fetchAmadeusToken, amadeusToken } = useContext(TequilaContext);
+ 
 
 
-const fetchSuggestionsDebounced = useCallback(debounce(async (value, inputName) => {
-  if (value.length > 2) {
-    try {
-      const apikey = "HxbcQFD8rtk3Ev13yGLVQDBa59E_coFX";
-      // Call suggestAirport and handle the returned data
-      const suggestions = await suggestAirport({ apikey, term: value });
-      console.log(suggestions)
-      setAirportSuggestions(suggestions); // Update state with the fetched data
-    } catch (error) {
-      console.error("Error fetching airport suggestions:", error);
-      setAirportSuggestions([]); // Ensure state is cleared or handled appropriately on error
-    }
-  } else {
-    setAirportSuggestions([]); 
-  }
-}, 500), []);
+    fetchAmadeusToken({ clientId: 'NJyhbOUCZAYtLotRqkAsv2iAotS1ll58', clientSecret: 'gfwe9d3YCOMbs6q1' });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setSearchParams(prevState => ({ ...prevState, [name]: value }));
-
-    // Call the debounced fetch function
-    fetchSuggestionsDebounced(value, name);
-  };
-  const handleSelectSuggestion = (suggestion) => {
-    setSearchParams(prevState => ({
-      ...prevState,
-      [prevState.destination.length <= 2 ? 'origin' : 'destination']: suggestion.code
-    }));
-    setAirportSuggestions([]); // Clear suggestions after selection
-  };
+    
 
   function debounce(func, wait) {
     let timeout;
@@ -90,44 +63,7 @@ const fetchSuggestionsDebounced = useCallback(debounce(async (value, inputName) 
     setCalendarOpen(!isCalendarOpen);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
-  
-    // Assuming `apikey` is defined and available
-    const apikey = "HxbcQFD8rtk3Ev13yGLVQDBa59E_coFX";
-  
-    // Extract parameters from the form's state
-    const { origin, destination, dateRange } = searchParams;
-    const dateFrom = format(dateRange.startDate, 'yyyy-MM-dd');
-    const dateTo = format(dateRange.endDate, 'yyyy-MM-dd');
-  
-    try {
-      const response = await searchItinery({
-        apikey,
-        flyFrom: origin,
-        flyTo: destination,
-        dateFrom,
-        dateTo,
-      });
-  
-      // Assuming you want to save the entire response for now; adjust as needed
-      setSearchParams(prevState => ({
-        ...prevState,
-        searchResponse: response.data, // Saving the array of flight options
-        bookingToken: response.data[0]?.booking_token // Saving booking token of the first flight option
-      }));
-  
-      // Optionally, navigate to another page or show a message
-      // console.log("Search successful", response);
 
-      // console.log(searchParams.searchResponse)
-      console.log(searchParams.bookingToken)
-    
-    } catch (error) {
-      console.error("Error searching itinerary:", error);
-      // Handle errors (e.g., show error message)
-    }
-  };
   
   
   const displayDateRange = () => {
@@ -140,7 +76,7 @@ const fetchSuggestionsDebounced = useCallback(debounce(async (value, inputName) 
 
   return (
     <div className={styles.searchFormWrapper}>
-      <form onSubmit={handleSubmit} className={styles.formContainer}>
+      <form className={styles.formContainer}>
         {/* Origin and Destination Input */}
         <div className={styles.inputWithIcon}>
           <FontAwesomeIcon icon={faPlaneDeparture} className={styles.inputIcon} />
@@ -149,7 +85,7 @@ const fetchSuggestionsDebounced = useCallback(debounce(async (value, inputName) 
             name="origin"
             placeholder="From where?"
             value={searchParams.origin}
-            onChange={handleInputChange}
+            
             className={styles.inputField}
           />
         </div>
@@ -160,7 +96,6 @@ const fetchSuggestionsDebounced = useCallback(debounce(async (value, inputName) 
             name="destination"
             placeholder="Where to?"
             value={searchParams.destination}
-            onChange={handleInputChange}
              className={styles.inputField}
           />
         </div>
@@ -170,7 +105,6 @@ const fetchSuggestionsDebounced = useCallback(debounce(async (value, inputName) 
               <div
                 key={suggestion.id}
                 className={styles.suggestionItem}
-                onClick={() => handleSelectSuggestion(suggestion)}
               >
                 {suggestion.name} ({suggestion.code})
               </div>
